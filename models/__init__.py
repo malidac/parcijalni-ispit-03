bb9b028ce7d4d53e811f3b90e1c77adc13adf4b4
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-04-04 20:46:42 +0200

    Summary

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from models.customer_model import Customer
 from models.product_model import Product, ProductItem
 from models.offer_model import Offer
+from models.user_model import User
--- services/user_service.py
+++ services/user_service.py
@@ -1,2 +1,23 @@
+import requests
+
+from models import User
+
+
 class UserService:
-    pass
+    def __init__(self,
+                 url: str = 'https://jsonplaceholder.typicode.com/users',
+                 user_id: int = 1):
+        self.url: str = url
+        self.user: User = None
+        self.user_id: int = user_id
+
+        self.get_user()
+
+    def get_user(self):
+        response = requests.get(f'{self.url}/{self.user_id}')
+        data = response.json()
+        self.user = User(
+            name = data['name'],
+            username = data['username'],
+            email = data['email']
+        )
